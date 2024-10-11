27f35a1451a9974dbe2dbfe7f975f841547fddc9
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-10-11 13:32:13 +0300

    modified config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'bY7Gm2P!qv4Z$kZ1s8JtXo3fNm9Hc2W#Jb5C6Qs7DpE')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'filesystem'
